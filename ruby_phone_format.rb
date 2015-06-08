30d244d642052cfa9094318f00f5f89ac10181e8
hpetrow <heather.petrow@gmail.com>
2015-06-08 15:43:09 -0400

    done

--- ruby_phone_format.rb
+++ ruby_phone_format.rb
@@ -1,1 +1,10 @@
 # Write your code here
+def normalize_phone_number(ph_number)
+  numbers = ph_number.scan(/\d/).join("")
+  if numbers.length != 10
+    result = ph_number
+  else
+    result = "(" << numbers[0..2] << ") " << numbers[3..5] << "-" << numbers[6..9]
+  end
+  result
+end
